1c5b7a68de8b33622fcf0ea10d6697e79e3df93d
Nurdin <50176673+nurdin73@users.noreply.github.com>
2019-11-23 16:52:41 +0700

    update 2.php

--- 2.php
+++ 2.php
@@ -2,7 +2,6 @@
 
 function hitungKembalian($total, $uang)
 {
-	$data = [50000,20000,10000,5000,2000,500];
 	$kembalian = $uang - $total;
 	$data1 = $kembalian % 50000;
 	$a = ($kembalian - $data1) / 50000;
